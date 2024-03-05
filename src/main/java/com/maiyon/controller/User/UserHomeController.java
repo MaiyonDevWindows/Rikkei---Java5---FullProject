67e393d95bd582021c3022346ff8c98e638a5030
MaiyonDevMacOSX <maiyonaisu1102@gmail.com>
2024-03-06 04:59:41 +0700

    FE - Java Module 5 Project - MaiyonDevMac

--- src/main/java/com/maiyon/controller/User/UserHomeController.java
+++ src/main/java/com/maiyon/controller/User/UserHomeController.java
@@ -1,15 +1,29 @@
 package com.maiyon.controller.User;
 
+import com.maiyon.model.entity.Category;
+import com.maiyon.model.entity.Product;
+import com.maiyon.service.CategoryService;
+import com.maiyon.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserHomeController {
-    @RequestMapping
-    public String userHome(){
+    private final CategoryService categoryService;
+    private final ProductService productService;
+    @GetMapping("/home")
+    public String userHome(Model model){
+        List<Category> categories = categoryService.getAll();
+        List<Product> products = productService.getAll();
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", products);
         return "user/index";
     }
 }
